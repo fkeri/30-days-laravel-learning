eb5bcac2d6d55b42f52f5db824add1398221dd3a
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-12 13:24:51 +0100

    Day 18: Editing, Updating, and Deleting a Job

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->paginate(3);
 
@@ -16,16 +17,19 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -39,6 +43,47 @@ Route::post('/jobs', function () {
         'employer_id' => 1,
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // 1. Validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // 2. TODO: Authorize
+
+    // 3. Update the job
+    $job = Job::findOrFail($id);
+    $job->title = request('title');
+    $job->salary = request('salary');
+
+    // 4. Persist
+    $job->save();
+
+    // 5. Redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // 1. TODO: Authorize
+
+    // 2. Delete the job
+    $job = Job::findOrFail($id);
+    $job->delete();
+
+    // 3. Redirect
     return redirect('/jobs');
 });
 
