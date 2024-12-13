d0433d29c254824c748a214f979a9b69d93e1054
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-13 12:40:37 +0100

    Day 22: add Login functionality

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -14,7 +15,24 @@ class SessionController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // 1. Validate
+        $attrs = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        // 2. Attempt to login the user
+        if (!Auth::attempt($attrs)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials do not match.'
+            ]);
+        }
+
+        // 3. On login success: regenerate session token
+        request()->session()->regenerate();
+
+        // 4. Redirect
+        return redirect('/jobs');
     }
 
     public function destroy()
