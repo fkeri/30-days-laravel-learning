6eb126f46c7cd9c736b79e45adc119a4218ff207
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-13 11:17:34 +0100

    Day 21: Refactor Create Job Form into Components

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
-
-Route::view('/contact', 'contact');
