1ff43fc21cda43ff00f926c48ae9ff73f4cdad1b
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-13 12:23:39 +0100

    Day 22: Store a new Registered User

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +16,21 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // 1. Validate
+        $attrs = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'], // 'confirmed' -> looks for 'password_confirmation'
+        ]);
+
+        // 2. Create the User
+        $user = User::create($attrs);
+
+        // 3. Login
+        Auth::login($user);
+
+        // 4. Redirect
+        return redirect('/jobs');
     }
 }
